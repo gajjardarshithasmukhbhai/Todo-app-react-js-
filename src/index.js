037e54abdaa9bf35c4de67fb7f21cf9cdd3609ca
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-08-22 11:39:47 -1200

    ddjj
    jjj

--- src/index.js
+++ src/index.js
@@ -12,6 +12,14 @@ class Darshit extends React.Component{
 		this.Task=this.Task.bind(this);
 		this.click=this.click.bind(this);
 		this.addTask=this.addTask.bind(this);
+		this.delete=this.delete.bind(this);
+		this.editTask=this.editTask.bind(this);
+	}
+	editTask(array,neww)
+	{
+		var Task=this.state.Task;
+		var Task=Task[array];
+		Task['name']=neww;
 	}
 	addTask(newValue)
 	{
@@ -26,7 +34,14 @@ class Darshit extends React.Component{
 			Task:this.state.Task,
 		})
 	}
-
+	delete(re)
+	{
+		let Task=this.state.Task;
+		Task.splice(re,1);
+		this.setState({
+			Task:Task,
+		})
+	}
 	click(event)
 	{
 		event.preventDefault();
@@ -44,38 +59,85 @@ class Darshit extends React.Component{
 		return(
 				<div>
 					<div class="todo">
+					<p class="p">v 1.2</p>
 						<form onSubmit={this.click}>
 						<input type="text" class="text" value={this.state.update} onChange={this.addTask}/>
 							<button type="button" class="add" onClick={this.click}>add+</button>
 						</form>
 						<div class="Todo">				
-							<ol class="li">
+							<ul class="li">
 							{
 								this.state.Task.map((name,index)=>{
-									return <Todo key={name.name}names={name} index={index}/>}
+									return <Todo key={name.name}names={name} index={index} delete={this.delete} edit={this.editTask}/>}
 									)
 							}
-							</ol>
+							</ul>
 						</div>
 					</div>
 				</div>
 		);
 	}
 }
-class Todo extends React.Component{
+class Todo extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state={
+			Edit:false,
+		}
+		this.toggle=this.toggle.bind(this);
+		this.reactfrom=this.reactfrom.bind(this);
+		this.reactDom=this.reactDom.bind(this);
+		this.updatechange=this.updatechange.bind(this);
+	}
+	toggle()
+	{
+		let {Edit}=this.state;
+		return(
+		this.setState({
+			Edit:!Edit,
+		})
+		)
+	}
+	updatechange(event)
+	{
+		event.preventDefault();
+    	this.props.edit(this.props.index,this.input.value);
+    	this.toggle();
+	}
+	reactfrom()
+	{
+		return(
+		<from onSubmit={this.updatechange}>
+			<input type="text" ref={(value)=>{this.input=value}} defaultValue={this.props.names.name}/>
+			<button onClick={this.updatechange}>Update</button>
+		</from>
+		)
+	}
+	reactDom()
+	{
+		return(
+		<li>{this.props.names.name}
+			<button type="button" class="remove" onClick={(event)=>{event.stopPropagation();this.props.delete(this.props.index)}}>remove</button>
+			<button type="button" class="Edit" onClick={(event)=>{event.stopPropagation();this.toggle()}}>Edit</button>
+		</li>	
+		)				
+	}
 	render()
 	{
-
-		if(this.props.names.name=='Gajjar Darshit'){
-			this.props.index+1;
-		}
+		let {Edit}=this.state;
 		return(
 				<div>
-					<li>{this.props.names.name}
-					<button type="" >delete</button>
-				</li>					
+				<br/>
+				<section>
+				{
+					Edit?this.reactfrom():this.reactDom()
+
+				}
+				</section>
 				</div>
 		);
 	}
 }
-ReactDOM.render(<Darshit/>,document.getElementById("root")) 
+ReactDOM.render(<Darshit/>,document.getElementById("root"));
