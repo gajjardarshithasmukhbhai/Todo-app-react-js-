cdf54f33fd5c7876ff78fbcabdb312207a60cf11
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-08-22 03:50:54 -1200

    ssjsj
    jjj

--- src/index.js
+++ src/index.js
@@ -2,14 +2,61 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css';
 class Darshit extends React.Component{
+	constructor(props)
+	{
+		super(props);
+		this.state={
+			Task:[{name:"Gajjar Darshit"}],
+			update:"Gajjar Darshit"
+		}
+		this.Task=this.Task.bind(this);
+		this.click=this.click.bind(this);
+		this.addTask=this.addTask.bind(this);
+	}
+	addTask(newValue)
+	{
+		newValue.preventDefault();
+		this.setState({
+			update:newValue.target.value,
+		})
+	}
+	Task()
+	{
+		this.setState({
+			Task:this.state.Task,
+		})
+	}
+
+	click(event)
+	{
+		event.preventDefault();
+		let Task=this.state.Task;
+		let update=this.state.update;
+		Task.push({
+			name:update,
+			})
+		this.setState({
+			Task:Task,
+		})
+	}
 	render()
 	{
 		return(
 				<div>
 					<div class="todo">
-						<input type="text" class="text"/>
-						<button type="button" class="add">add+</button>
-						<Todo/>
+						<form onSubmit={this.click}>
+						<input type="text" class="text" value={this.state.update} onChange={this.addTask}/>
+							<button type="button" class="add" onClick={this.click}>add+</button>
+						</form>
+						<div class="Todo">				
+							<ol class="li">
+							{
+								this.state.Task.map((name,index)=>{
+									return <Todo key={name.name}names={name} index={index}/>}
+									)
+							}
+							</ol>
+						</div>
 					</div>
 				</div>
 		);
@@ -18,12 +65,15 @@ class Darshit extends React.Component{
 class Todo extends React.Component{
 	render()
 	{
+
+		if(this.props.names.name=='Gajjar Darshit'){
+			this.props.index+1;
+		}
 		return(
 				<div>
-					<div class="Todo">
-					</div>
+					<li>{this.props.names.name}</li>
 				</div>
 		);
 	}
 }
-ReactDOM.render(<Darshit/>,document.getElementById("root"))  
+ReactDOM.render(<Darshit/>,document.getElementById("root")) 
