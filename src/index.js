bda952ad505ace0525d582e14b61b4facce05114
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-08-24 08:56:36 -1200

    dddh
    hh

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import  'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 class Darshit extends React.Component{
 	constructor(props)
